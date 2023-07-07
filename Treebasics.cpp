c27a37260884f9bbbcd68fdfe4192eece12fce1d
Shubham Diwakar <54909131+Shubham4359@users.noreply.github.com>
2023-07-07 14:27:25 +0530

    Update Treebasics.cpp

--- Treebasics.cpp
+++ Treebasics.cpp
@@ -11,6 +11,20 @@ class node{
         right=NULL;
     }
 };
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
 node* buildtree(){
     int d;
     cin >> d;
@@ -61,8 +75,180 @@ void level(int cur,node* root){
         cout<<root->data<<" ";
         return;
     }
-    if(root->left!=NULL){level(cur-1,root->left);}
-    if(root->right!=NULL){level(cur-1,root->right);}
+    level(cur-1,root->left);
+    level(cur-1,root->right);
+}
+void levelorder(node* root){
+    queue<node*>q;
+    q.push(root);
+    while(!q.empty()){
+        int n=q.size();
+        for(int i=0;i<n;i++){
+        node* cur_root=q.front();
+        q.pop();
+        if(cur_root!=NULL){
+            cout<<cur_root->data<<" ";
+            q.push(cur_root->left);
+            q.push(cur_root->right);
+        }
+        }
+        cout<<"\n";
+    }
+}
+int countnodes(node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int cnt=1;
+    cnt+=countnodes(root->left);
+    cnt+=countnodes(root->right);
+    return cnt;
+}
+int sumnodes(node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int sum=root->data;
+    sum+=sumnodes(root->left);
+    sum+=sumnodes(root->right);
+    return sum;
+}
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   vector<int>ans;
+   if(root==NULL){
+       return ans;
+   }
+   queue<Node*>q;
+   q.push(root);
+   while(!q.empty()){
+       int n=q.size();
+       for(int i=0;i<n;i++){
+        Node* cur=q.front();
+        q.pop();
+        if(i==0){
+            ans.push_back(cur->data);
+        }
+        if(cur->left!=NULL){
+            q.push(cur->left);
+        }
+        if(cur->right!=NULL){
+            q.push(cur->right);
+        }
+       }
+   }
+   return ans;
+}
+void preorder_leftview(vector<int>&ans,Node* root,int level,int &max_level){
+    if(root==NULL){
+        return ;
+    }
+    if(level>max_level){
+        ans.push_back(root->data);
+        max_level=level;
+    }
+    preorder_leftview(ans,root->left,level+1,max_level);
+    preorder_leftview(ans,root->right,level+1,max_level);
+}
+ vector<int> rightView(Node *root)
+    {
+       // Your code here
+   vector<int>ans;
+   if(root==NULL){
+       return ans;
+   }
+   queue<Node*>q;
+   q.push(root);
+   while(!q.empty()){
+       int n=q.size();
+       for(int i=0;i<n;i++){
+        Node* cur=q.front();
+        q.pop();
+        if(i==n-1){
+            ans.push_back(cur->data);
+        }
+        if(cur->left!=NULL){
+            q.push(cur->left);
+        }
+        if(cur->right!=NULL){
+            q.push(cur->right);
+        }
+       }
+   }
+   return ans;
+    }
+vector<int> topView(Node *root){
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        map<int,int>mp;
+        //at dis=d first node to be visible;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            Node* cur=q.front().first;
+            int dis=q.front().second;
+            q.pop();
+            if(cur==NULL){
+                continue;
+            }
+            if(mp.find(dis)==mp.end()){
+                mp[dis]=cur->data;
+            }
+                q.push({cur->left,dis-1});
+                q.push({cur->right,dis+1});
+        }
+        for(auto x:mp){
+            ans.push_back(x.second);
+        }
+        return ans;
+}
+vector<int> bottomView(Node *root)
+    {
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        map<int,int>mp;
+        //at dis=d first node to be visible;
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        while(!q.empty()){
+            Node* cur=q.front().first;
+            int dis=q.front().second;
+            q.pop();
+            if(cur==NULL){
+                continue;
+            }
+                mp[dis]=cur->data;
+                q.push({cur->left,dis-1});
+                q.push({cur->right,dis+1});
+        }
+        for(auto x:mp){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+TreeNode* invertTree(TreeNode* root) {
+     if(root!=NULL){
+      TreeNode* temp=root->left;
+     root->left=root->right;
+     root->right=temp;
+     invertTree(root->left);
+     invertTree(root->right);   
+     }
+     return root;
+}
+int diameter(node* root,int &max_dia){
+    if(root==NULL){
+        return 0;
+    }
+    int c_left=diameter(root->left,max_dia);
+    int c_right=diameter(root->right,max_dia);
+    max_dia=max(max_dia,c_left+c_right+1);
+    return max(c_left,c_right)+1;
 }
 int main(){
   node* root = buildtree();
@@ -72,14 +258,19 @@ int main(){
   cout<<"\n";
   inorder(root);
   */
-  int h=height(root);
+  /*int h=height(root);
   cout<<h<<"\n";
   for(int i=0;i<h;i++){
       level(i,root);
       cout<<"\n";
   }
+  */
+  /*levelorder(root);
+  cout<<countnodes(root)<<"\n";
+  cout<<sumnodes(root)<<"\n";
+  */
+  int max_diameter=0;
+  diameter(root,max_diameter);
+  cout<<max_diameter<<"\n";
     return 0;
 }
-
-
-
