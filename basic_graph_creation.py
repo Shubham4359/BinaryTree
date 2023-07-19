a7e0b4ce5045d9c03e8868d03c5e58e41ec0d1b4
Shubham Diwakar <54909131+Shubham4359@users.noreply.github.com>
2023-07-20 01:18:30 +0530

    Update basic_graph_creation.py

--- basic_graph_creation.py
+++ basic_graph_creation.py
@@ -13,6 +13,31 @@ def removeEdge(i,j,graph):
     graph[i].remove(j)
     graph[j].remove(i)
     
+def bfs(graph,src):
+    # Create a queue for BFS
+    queue = []
+    vis = [0]*(n+1) 
+    # Add the source node in
+    # visited and enqueue it
+    queue.append(src)
+    vis[src]=1
+ 
+    while queue:
+        # Dequeue a vertex from
+        # queue and print it
+        s = queue.pop(0)
+        print (s, end = " ")
+ 
+        # Get all adjacent vertices of the
+        # dequeued vertex s. If a adjacent
+        # has not been visited, then add it
+        # in visited and enqueue it
+        for i in graph[s]:
+            if vis[i]==0:
+                queue.append(i)
+                vis[i]=1
+                
+    
 if __name__ == '__main__':
      
     #  n is the number of vertices
@@ -34,4 +59,5 @@ if __name__ == '__main__':
     
     print(adjMat)
     printGraph(graph)
+    bfs(graph,1)
     
